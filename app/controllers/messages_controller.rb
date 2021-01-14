4d3186e71e276ac8f991797cdb2e8eebb6be2cdb
sogu-rep <shunogu1109@gmail.com>
2021-01-14 13:35:08 +0900

    Completed ajax function

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,35 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: %i[destroy edit update]
+
   def index
+    @messages = Message.all.order(:id)
   end
+
+  def new 
+  end
+
+  def create
+    @message = Message.create!(message_params)
+  end
+
+  def edit
+  end
+
+  def update
+    @message.update!(message_params)
+  end
+
+  def destroy
+    @message.destroy!
+  end
+
+  private
+
+    def message_params
+      params.require(:message).permit(:content)
+    end
+
+    def set_message
+      @message = Message.find(params[:id])
+    end
 end
